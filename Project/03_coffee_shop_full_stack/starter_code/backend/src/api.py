24552fa38463d57c6dbdc799e35780e9f1255267
tharunkumars <tharunkumars@gmail.com>
2024-07-16 23:31:08 +0530

    IAM Started Backend Server Running

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
-from flask_cors import CORS
+
+from flask_cors import CORS, cross_origin
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
